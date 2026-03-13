5340a6e838866a5a8936042d4b55e59c30b1aafc
23701A04H1 <kalavaruchitha1@gmail.com>
2026-03-13 11:05:02 +0530

    Add files via upload

---
+++ java script/array.js
@@ -0,0 +1,20 @@
+// let phones=['apple','oneplus','samsung'];
+// console.log(phones[2]);
+// console.log(phones[0]);
+
+// let arr=[1,2,3];
+// arr.push(4);
+// console.log(arr);
+
+
+let arr=[1,2,3];
+arr.shift(2);
+console.log(arr);
+
+// let arr=[1,2,3];
+// arr.unshift(2);
+// console.log(arr);
+
+
+// let arr=[1,2,3];
+// console.log(arr.length);
